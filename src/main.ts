ce087bc5f5ef049ca5d3303dc2dbdce53751c481
lukeramsden <lukeramsden@protonmail.com>
2020-06-01 11:07:01 +0100

    Exported some draft-js types from main

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
 import DraftDisplay from './DraftDisplay';
 
+export {
+  RawDraftContentState as DraftContentState,
+  DraftBlockType,
+  RawDraftContentBlock as DraftContentBlock,
+} from 'draft-js';
 export default DraftDisplay;
